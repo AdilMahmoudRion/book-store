d20e046bbee669eed4ab179e9a922ca4bc4a11b1
https://github.com/AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-09-02 23:03:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+document.getElementById("error-message").style.display = "none";
+// document.getElementById('blank-input').style.display = 'none';
+const searchBook = () => {
+  const searchFiled = document.getElementById("search-filed");
+  const searchBookName = searchFiled.value;
+  console.log(searchBookName);
+  //   searchFiled.value = " ";
+  const url = `https://openlibrary.org/search.json?q=${searchBookName}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => searchBookList(data.docs))
+    .catch((error) => errorMessage(error));
+};
+const errorMessage = () => {
+  document.getElementById("error-message").style.display = "block";
+};
+const searchBookList = (booksName) => {
+  const searchResult = document.getElementById("search-result");
+  searchResult.innerHTML = " ";
+  // search result are
+  const searchCount = document.getElementById("search-count");
+  searchCount.innerHTML = " ";
+  const p = document.createElement("p");
+  p.innerHTML = `
+            <p class= "fs-6 fw-light text-muted">About: ${booksName.length} results </p>
+    `;
+  searchCount.appendChild(p);
+
+  // no result found
+  const blankSearch = document.getElementById("blank-search");
+  blankSearch.innerHTML = " ";
+  if (booksName.length === 0) {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `
+        <h2 class="text-center">No result Found</h2>
+        `;
+    blankSearch.appendChild(h2);
+  }
+
+  // search result are
+  booksName.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    div.innerHTML = `
+            <div class="card">
+                    <img src="${imgUrl}" class="card-img-top w-100" alt="...">
+                <div class="card-body mt-3">
+                    <h2 class="card-title">${book.title.slice(0, 50)}</h2>
+                     <h6>
+                     Author name: ${book.author_name}
+                     </h6>
+                     <h6>
+                     Publisher: ${book.publisher}
+                     </h6>
+                     <h6 class= "fs-6 fw-light text-muted">
+                     First publish date: ${book.first_publish_year}
+                     </h6>
+                </div>
+            </div>
+        `;
+    searchResult.appendChild(div);
+  });
+};
